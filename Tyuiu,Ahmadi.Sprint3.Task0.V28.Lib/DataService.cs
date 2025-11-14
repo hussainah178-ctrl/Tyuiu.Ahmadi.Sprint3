e95b326d7a4e06fefea0b6912d7375d5703c3680
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-13 22:37:57 -0800

    Add project files.

---
+++ Tyuiu,Ahmadi.Sprint3.Task0.V28.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.Ahmadi.Sprint3.Task0.V28.Lib
+{
+    public class DataService : ISprint3Task0V28
+    {
+        public double GetMultiplySeries(double x)
+        {
+            double p = 1;
+            for (int i = 1; i <= 17; i++)
+            {
+                p *= (Math.Pow(x, 3) + i) + 2;
+            }
+            return Math.Round(p, 3);
+        }
+
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tyuiu,Ahmadi.Sprint3.Task0.V28.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.Ahmadi.Sprint3.Task0.V28.Lib;
+
+namespace Tyuiu.Ahmadi1.Sprint3.Task0.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            double result = ds.GetMultiplySeries(0.25);
+            Assert.AreEqual(411589.537, result);
+        }
+    }
+}
